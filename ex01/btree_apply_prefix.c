25c7b92ad167c25170268aa73ec9ab9ca37f468a
Matthew Gould <matt@everettgould.com>
2016-11-11 13:00:31 -0800

    was calling the function by the wrong name, doh

--- ex01/btree_apply_prefix.c
+++ ex01/btree_apply_prefix.c
@@ -6,7 +6,7 @@
 /*   By: mgould <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 21:08:08 by mgould            #+#    #+#             */
-/*   Updated: 2016/11/11 12:57:58 by mgould           ###   ########.fr       */
+/*   Updated: 2016/11/11 13:00:03 by mgould           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void btree_apply_prefix(t_btree *root, void (*applyf)(void *))
 	t_btree	*box;
 
 	box = root;
-	box->item = f(box->item);
+	box->item = applyf(box->item);
 	if (box->left)
 	{
 		btree_apply_prefix(box->left, applyf);
