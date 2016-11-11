2190eda58aa2657ebb8ebafd44cd97b9704dc8c5
Matthew Gould <matt@everettgould.com>
2016-11-11 13:12:36 -0800

    finishing apply uffix

---
+++ ex03/btree_apply_suffix.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   btree_apply_suffix.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgould <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/11 13:10:54 by mgould            #+#    #+#             */
+/*   Updated: 2016/11/11 13:11:10 by mgould           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_btree.h"
+
+void btree_apply_suffix(t_btree *root, void (*applyf)(void *))
+{
+	t_btree	*box;
+
+	box = root;
+	if (box->left)
+	{
+		btree_apply_suffix(box->left, applyf);
+	}
+	if (box->right)
+	{
+		btree_apply_suffix(box->right, applyf);
+	}
+	box->item = applyf(box->item);
+	return ;
+}
