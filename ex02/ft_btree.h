2aca300e1843817286fe70789835c1cbb57e0a17
Matthew Gould <matt@everettgould.com>
2016-11-11 13:05:57 -0800

    updating .h files to remove stdlib library that I was using in exercise 00 and left in there by mistake

--- ex02/ft_btree.h
+++ ex02/ft_btree.h
@@ -6,13 +6,12 @@
 /*   By: mgould <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 20:14:26 by mgould            #+#    #+#             */
-/*   Updated: 2016/11/11 12:50:14 by mgould           ###   ########.fr       */
+/*   Updated: 2016/11/11 13:04:35 by mgould           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_BTREE_H
 # define FT_BTREE_H
-# include <stdlib.h>
 
 typedef	struct		s_btree
 {
--- ex03/ft_btree.h
+++ ex03/ft_btree.h
@@ -6,7 +6,7 @@
 /*   By: mgould <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 20:14:26 by mgould            #+#    #+#             */
-/*   Updated: 2016/11/11 13:04:52 by mgould           ###   ########.fr       */
+/*   Updated: 2016/11/11 13:05:06 by mgould           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ typedef	struct		s_btree
 	struct s_btree	*right;
 	void			*item;
 }					t_btree;
-void				b_tree_apply_prefix(t_btree *root, void (*applyf)(void *));
+void				btree_apply_suffix(t_btree *root, void (*applyf)(void *));
 #endif
