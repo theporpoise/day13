69f68f3d0bd4b5b350bf948b81c93ca488db1536
Matthew Gould <matt@everettgould.com>
2016-11-11 12:47:03 -0800

    updating to have .h file included

--- ex01/btree_apply_prefix.c
+++ ex01/btree_apply_prefix.c
@@ -6,9 +6,11 @@
 /*   By: mgould <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 21:08:08 by mgould            #+#    #+#             */
-/*   Updated: 2016/11/11 11:53:56 by mgould           ###   ########.fr       */
+/*   Updated: 2016/11/11 12:46:45 by mgould           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "ft_btree.h"
 
 void btree_apply_prefix(t_btree *root, void (*applyf)(void *))
 {
