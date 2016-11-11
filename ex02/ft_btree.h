d2130faadee910a4cd114f2f6f795192566115d7
Matthew Gould <matt@everettgould.com>
2016-11-11 12:50:36 -0800

    updating header file, learning how to set these up correctly, takes practice

---
+++ ex02/ft_btree.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_btree.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgould <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/10 20:14:26 by mgould            #+#    #+#             */
+/*   Updated: 2016/11/11 12:50:14 by mgould           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BTREE_H
+# define FT_BTREE_H
+# include <stdlib.h>
+
+typedef	struct		s_btree
+{
+	struct s_btree	*left;
+	struct s_btree	*right;
+	void			*item;
+}					t_btree;
+void				btree_apply_infix(t_btree *root, void (*applyf)(void *));
+#endif
