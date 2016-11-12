b7e998e14fdbeccafef617d2136217d32a954d8d
Matthew Gould <matt@everettgould.com>
2016-11-11 17:30:07 -0800

    normed

--- ex04/btree_insert_data.c
+++ ex04/btree_insert_data.c
@@ -6,18 +6,19 @@
 /*   By: mgould <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/11 13:10:54 by mgould            #+#    #+#             */
-/*   Updated: 2016/11/11 13:46:19 by mgould           ###   ########.fr       */
+/*   Updated: 2016/11/11 17:29:47 by mgould           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_btree.h"
 
-void btree_insert_data(t_btree **root, void *item, int (*cmpf)(void *, void *))
+void	btree_insert_data(t_btree **root, void *item, \
+		int (*cmpf)(void *, void *))
 {
 	t_btree	*box;
 
 	box = *root;
-	if (box == NULL)
+	if (box == 0)
 	{
 		box = btree_create_node(item);
 		return ;
@@ -27,7 +28,7 @@ void btree_insert_data(t_btree **root, void *item, int (*cmpf)(void *, void *))
 		box = box->right;
 		btree_insert_data(&box, item, cmpf);
 	}
-	if i(cmpf((box->item), item) >= 0)
+	if (cmpf((box->item), item) >= 0)
 	{
 		box = box->left;
 		btree_insert_data(&box, item, cmpf);
